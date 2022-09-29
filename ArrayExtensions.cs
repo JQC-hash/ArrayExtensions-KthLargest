22cd9058fb400c9d961915be7115d4935cbba11f
Jia-Qi Chen <78511732+JQC-hash@users.noreply.github.com>
2022-09-29 21:22:08 +1000

    Create ArrayExtensions.cs

---
+++ ArrayExtensions.cs
@@ -0,0 +1,40 @@
+public static class ArrayExtensions{
+    public static void Swap(this int[] arr, int index1, int index2){
+        if (index1 >= 0 && index1 < arr.Length && index2 >= 0 && index2 < arr.Length){
+            int temp = arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = temp;
+        }
+    }
+    
+    public static int KthLargestElement(this int[] arr, int start, int end, int k){
+        int index = arr.Partition(start, end);
+
+        if (index == arr.Length - k)
+            return arr[index];
+        else if (index < arr.Length - k)
+            return arr.KthLargestElement(index + 1, end, k);
+        else
+            return arr.KthLargestElement(start, index, k);
+    }
+
+    private static int Partition(this int[] arr, int start, int end){
+        int j = new Random().Next(start, end + 1);    
+        arr.Swap(start, j);
+
+        int left = start;
+        int right = end;
+        while (left < right){
+            if (arr[left + 1] < arr[left]){
+                arr.Swap(left + 1, left);
+                left++;
+            }
+            else if (arr[right] >= arr[left])
+                right--;
+            else
+                arr.Swap(left + 1, right);
+        }
+
+        return left;
+    }
+}
